e7d76caf5ee2f9d28adfa2a8064759107d1b5423
Jinnan Wang <jnwang@linx-info.com>
2014-08-27 14:43:52 +0800

    在远程建立一个taskdir用于存放task所用数据         taskdir 目录结构如下         tmp/testcase         tmp/testlist         tmp/result

--- lib/libs.sh
+++ lib/libs.sh
@@ -21,6 +21,9 @@
 set -o nounset                              # Treat unset variables as an error
 
 REMOTE_DIR="/tmp"
+REMOTE_DIR_testcase="/tmp/testcase"
+REMOTE_DIR_testlist="/tmp/testlist"
+REMOTE_DIR_result="/tmp/result"
 
 context_check_tasklist()
 {
@@ -57,12 +60,27 @@ check_remote_disk_space()
 # $2: $USER
 # $3: $PASSWORD
 
-        disk_usage=$(sshpass -p$PASSWORD ssh $USER@$IP "LANG=C df -hm ${REMOTE_DIR} | tail -n 1" < /dev/null)
+        disk_usage=$(sshpass -p$3 ssh $2@$1 "LANG=C df -hm ${REMOTE_DIR} | tail -n 1" < /dev/null)
         #disk_usage=`LANG=C df -hm ${REMOTE_DIR} | tail -n 1`
         free_space=`echo $disk_usage | awk -F " " '{print $4}'`
 
         if [ $free_space -lt 20 ];then
                 echo "remote host '$IP' have not enough disk space on ${REMOTE_DIR} for runing task" >&2
+                exit 1;
+        fi
+
+        return 0
+}
+_create_taskdir_to_remote_host()
+{
+# $1: $IP
+# $2: $USER
+# $3: $PASSWORD
+        cmd="sshpass -p$3 ssh $2@$1 \"\"mkdir -p ${REMOTE_DIR};mkdir -p ${REMOTE_DIR_testcase};mkdir -p ${REMOTE_DIR_testlist};mkdir -p ${REMOTE_DIR_result}\"\""
+
+        ${cmd} </dev/null
+        if [ $? -ne 0 ];then
+                echo "_create_taskdir_to_remote_host to $1 as $2 with $3 create taskdir fail"
                 exit 1;
         fi
 
@@ -88,19 +106,21 @@ ssh_remote_scp()
 {
         # $1: file need to be transfered
         # $2: ipgroup file
-        cat $2|while read textline
+        while read textline
         do
                 if [ "x" == "x${textline}" ] ;then
                         continue
                 fi
+                
                 IP=`echo $textline |  awk -F "," '{print $1}'`
                 USER=`echo $textline |  awk -F "," '{print $2}'`
                 PASSWORD=`echo $textline |  awk -F "," '{print $3}'`
                 echo $IP $USER $PASSWORD 
                 check_remote_disk_space $IP $USER $PASSWORD
+                _create_taskdir_to_remote_host $IP $USER $PASSWORD
                 _scp_file_to_remote_host $IP $USER $PASSWORD $1
 
-        done
+        done<$2
 }
 file_transfer()
 {
