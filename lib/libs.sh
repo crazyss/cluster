58def3930432c22beeee27cdb25021f32b42a43c
Jinnan Wang <jnwang@linx-info.com>
2014-08-27 14:20:07 +0800

    现在它可以将指定的文件传输到远程

--- lib/libs.sh
+++ lib/libs.sh
@@ -20,6 +20,8 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+REMOTE_DIR="/tmp"
+
 context_check_tasklist()
 {
         return 0
@@ -54,22 +56,51 @@ check_remote_disk_space()
 # $1: $IP
 # $2: $USER
 # $3: $PASSWORD
-disk_usage=`sshpass -p $PASSWORD ssh $USER@$IP "df -hm /tmp"`
-echo $disk_usage | awk -F "" '{print $4}'
-        
+
+        disk_usage=$(sshpass -p$PASSWORD ssh $USER@$IP "LANG=C df -hm ${REMOTE_DIR} | tail -n 1" < /dev/null)
+        #disk_usage=`LANG=C df -hm ${REMOTE_DIR} | tail -n 1`
+        free_space=`echo $disk_usage | awk -F " " '{print $4}'`
+
+        if [ $free_space -lt 20 ];then
+                echo "remote host '$IP' have not enough disk space on ${REMOTE_DIR} for runing task" >&2
+                exit 1;
+        fi
+
+        return 0
+}
+_scp_file_to_remote_host()
+{
+# $1: $IP
+# $2: $USER
+# $3: $PASSWORD
+# $4: $file
+        cmd="sshpass -p$3 scp $4 $2@$1:${REMOTE_DIR}"
+
+        $cmd
+        if [ $? -ne 0 ];then
+                echo "_scp_file_to_remote_host to $1 as $2 with $3 copy $4 fail"
+                exit 1;
+        fi
+
+        return 0
 }
 ssh_remote_scp()
 {
-# $1: file need to be transfered
-# $2: ipgroup file
-        while read textline
+        # $1: file need to be transfered
+        # $2: ipgroup file
+        cat $2|while read textline
         do
-        IP=`echo $textline |  awk -F "," '{print $1}'`
-        USER=`echo $textline |  awk -F "," '{print $2}'`
-        PASSWORD=`echo $textline |  awk -F "," '{print $3}'`
-        echo $IP $USER $PASSWORD 
-        check_remote_disk_space $IP $USER $PASSWORD&
-        done <$2
+                if [ "x" == "x${textline}" ] ;then
+                        continue
+                fi
+                IP=`echo $textline |  awk -F "," '{print $1}'`
+                USER=`echo $textline |  awk -F "," '{print $2}'`
+                PASSWORD=`echo $textline |  awk -F "," '{print $3}'`
+                echo $IP $USER $PASSWORD 
+                check_remote_disk_space $IP $USER $PASSWORD
+                _scp_file_to_remote_host $IP $USER $PASSWORD $1
+
+        done
 }
 file_transfer()
 {
